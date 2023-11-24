07859ee2aeaebb8afe841ad26b091fe7b592e901
Ovi22102611 <junaedrahman8@gmail.com>
2023-11-24 09:21:08 +0000

    Add project files.

---
+++ RazorApplication/Pages/Index.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RazorApplication.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public string Name { get; set; }
+
+        public void OnGet()
+        {
+        }
+
+        public void OnPost(string name)
+        {
+            Name = name;
+        }
+    }
+}
